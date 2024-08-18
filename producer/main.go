2c7f0d2e8a6211e58df7c60acc12cdfa70ba373f
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 10:11:46 +0700

    Add producer kafka go🫸🌀✏️📗🐧🐳

---
+++ producer/main.go
@@ -0,0 +1,31 @@
+package producer
+
+import (
+	"context"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+func main() {
+	opts := []kgo.Opt{
+		kgo.SeedBrokers("localhost:9092"),
+		kgo.DefaultProduceTopic("my-topic"),
+		kgo.ClientID("producer-client-id"),
+	}
+
+	client, err := kgo.NewClient(opts...)
+	if err != nil {
+		// TODO: handle/log this error
+		return
+	}
+	defer client.Close()
+
+	record := &kgo.Record{
+		Value: []byte("Hello World"),
+		Topic: "my-topic",
+	}
+
+	if err := client.ProduceSync(context.Background(), record).FirstErr(); err != nil {
+		// TODO: handle/log this error
+		return
+	}
+}
