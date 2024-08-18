3769b43c8dc8984136131b0a8a4dc8152a1fe5a1
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 10:34:37 +0700

    Add setupProducer🫸🌀✏️📗🐧🐳

--- cmd/producer/producer.go
+++ cmd/producer/producer.go
@@ -105,3 +105,14 @@ func sendMessageHandler(producer sarama.SyncProducer,
 		})
 	}
 }
+
+func setupProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer([]string{KafkaServerAddress},
+		config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup producer: %w", err)
+	}
+	return producer, nil
+}
