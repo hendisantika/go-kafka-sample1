234a595bac209e32f588eca9767f912f7900759f
hendisantika <hendisantika@yahoo.co.id>
2024-08-18 10:33:14 +0700

    Add ErrUserNotFoundInProducer🫸🌀✏️📗🐧🐳

---
+++ cmd/producer/producer.go
@@ -0,0 +1,12 @@
+package main
+
+import "errors"
+
+const (
+	ProducerPort       = ":8080"
+	KafkaServerAddress = "localhost:9092"
+	KafkaTopic         = "notifications"
+)
+
+// ============== HELPER FUNCTIONS ==============
+var ErrUserNotFoundInProducer = errors.New("user not found")
